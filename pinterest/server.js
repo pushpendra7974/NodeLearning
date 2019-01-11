f6a737c32d64fa3507df9d5f701df8292591beaf
pushpendra7974 <pushpendra7974@hotmail.com>
2019-01-11 17:47:15 +0530

    Code of Connection to database and routing added

--- pinterest/server.js
+++ pinterest/server.js
@@ -15,4 +15,41 @@ var morgan = require('morgan');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
 var fileUpload = require('express-fileupload');
+var mongoose = require('mongoose');
 
+var app = express();
+
+// Database section start from here
+/**
+ *  Here I am using mongodb. If you want to use the same then
+ *  Goto www.mlab.com and signup for free account and create one database
+ *  and add one user to that database.
+ *  
+ *  dburl: contains the database connection string including username and password.
+ */
+
+var dburl ='mongodb://<username>:<userpassword>@ds153814.mlab.com:53814/pinterest';
+mongoose.connect(dburl,{useNewUrlParser: true},function(err){
+    if(err){
+        console.log('Error while connecting to database '+err);
+    }else{
+        console.log('Successfully connected to database...');
+    }
+})
+// Database connectivity ends here
+
+// Routing Methods Start from here
+
+    app.get('/',function(req,res,next){
+        res.json('Hello from the home page of this application!');
+    })
+// Routing Method Ends Here
+
+// Function to make your server listen at port no. 8090
+app.listen(8090,function(err){
+    if(err){
+        console.log('Error while listening to this port ' +err);
+    }else{
+        console.log('Server is running and listening to port no. 8090.');
+    }
+})
